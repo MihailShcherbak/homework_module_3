25a2ac9375ad9bd8d6f0dcc8580f30d8d6532500
Mikhail <pochta.mihaila@gmail.com>
2024-12-05 10:54:22 +0300

    Module_3 05.12.2024

--- module_3_1.py
+++ module_3_1.py
@@ -1,2 +1,19 @@
-print(5)
-print(10)
+calls = 0
+
+def count_calls():
+    global calls
+    calls = calls + 1
+
+def string_info(string):
+    count_calls()
+    return (len(string), string.upper(), string.lower())
+
+def is_contains(string, list_to_search):
+    count_calls()
+    return string.lower() in [elem.lower() for elem in list_to_search]
+
+print(string_info('Capybara'))
+print(string_info('Armageddon'))
+print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN'])) # Urban ~ urBAN
+print(is_contains('cycle', ['recycling', 'cyclic'])) # No matches
+print(calls)
