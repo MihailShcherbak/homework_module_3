50d0726145f673e5d94ce1708af41c69bb571934
Mikhail <pochta.mihaila@gmail.com>
2024-12-10 13:26:21 +0300

    Module_3 09.12.2024

--- module_3_5.py
+++ module_3_5.py
@@ -1,10 +1,10 @@
 def get_multiplied_digits(number):
     str_number = str(number)
-    while str_number[-1] == '0':
-        str_number = str_number[:-1]
-        break
     first = int(str_number[0])
     if len(str_number) > 1:
+        while str_number[-1] == '0':
+            str_number = str_number[:-1]
+            continue
         return first * get_multiplied_digits(int(str_number[1:]))
     else:
         return first
@@ -15,3 +15,7 @@ result2 = get_multiplied_digits(402030)
 print(result2)
 result3 = get_multiplied_digits(4008090)
 print(result3)
+result4 = get_multiplied_digits(1400000)
+print(result4)
+result5 = get_multiplied_digits(000000)
+print(result5)
